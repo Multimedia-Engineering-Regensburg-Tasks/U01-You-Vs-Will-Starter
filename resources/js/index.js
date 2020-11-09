a8e6b2b7c05df418124240e56e58a8f8209e6d1f
Alexander Bazo <alexanderbazo@googlemail.com>
2020-11-09 18:15:10 +0100

    Add comment to init sequence

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,11 +1,20 @@
 /* eslint-env browser */
 
+const DATA_PATH = "data/romeo-and-juliet-word-list.json";
+
 function loadWordlistFromJSON(callback) {
-    fetch(Config.dataPath).then(response => response.json()).then(data => callback(data));
+    fetch(DATA_PATH).then(response => response.json()).then(data => callback(data));
+}
+
+function init() {
+    loadWordlistFromJSON(onWordlistAvailable);
 }
 
 function onWordlistAvailable(wordlist) {
-    words = wordlist;
+    console.log("### WordList received ###");
+    // Prints the first 10 entries from the received word list
+    console.log(wordlist.slice(0, 10));
+    // Start your own implementation here
 }
 
-loadWordlistFromJSON(onWordlistAvailable);
+init();
